14c1eb7c5f42092c264dec0638e2bdf7a18dd889
Jazmine Singh <jzmn65@gmail.com>
2025-11-30 17:07:00 -0500

    Initial Commit

---
+++ WayCup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WayCup
+//
+//  Created by Jazmine Singh on 11/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WayCup/WayCupApp.swift
@@ -0,0 +1,17 @@
+//
+//  WayCupApp.swift
+//  WayCup
+//
+//  Created by Jazmine Singh on 11/30/25.
+//
+
+import SwiftUI
+
+@main
+struct WayCupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
